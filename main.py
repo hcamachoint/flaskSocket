43d16936ac957b168dfd1037fb0e057e0c7d2ea0
Hugo Camacho <hcamacho.int@gmail.com>
2024-09-06 14:31:21 -0400

    feat: origin check

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, ConnectionRefusedError, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+allow_origin_list = ['https://localhost', 'http://localhost', '127.0.0.1']
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/sender', methods=['GET'])
+@app.route('/sender', methods=['POST'])
 def sender():
-    emit('my response', {'data': "Sended to clients"}, namespace='/', broadcast=True)
+    json = request.get_json(force=True)
+    data = "%s" % (json)
+    print(data)
+    emit('my response', {'data': data}, namespace='/', broadcast=True)
     return jsonify({'msg': "Request sended to all clients connected"})
 
 @socketio.on('my event')
@@ -24,12 +28,12 @@ def test_message(message):
     emit('my response', {'data': message['data']}, broadcast=True)
 
 @socketio.on('connect')
-def test_connect(auth):
-    if auth['X-My-Auth'] != "110168078":
+def test_connect():
+    if request.environ['REMOTE_ADDR'] in allow_origin_list:
+        print('Client connected')
+        emit('my response', {'data': 'Connected'})
+    else:
         raise ConnectionRefusedError('unauthorized!')
-    
-    print('Client connected')
-    emit('my response', {'data': 'Connected'})
 
 @socketio.on('disconnect')
 def test_disconnect():
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
 virtualenv -p python3 env
 source env/bin/activate
 pip3 install -r requirements.txt
-flask --app main run
+flask --app main run --debug
